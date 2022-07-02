c12eca5f9073cc568b14b14a15aee03bcf43cf4f
2s2e <stevendiwenshi1113@gmail.com>
2022-07-01 22:59:58 -0700

    Solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(typeof(number) !== "number") {
+        number = parseInt(number);
+    }
+    if(number < 1) {
+        return 'OOPS';
+    }
+    if (number === 1) { 
+        return 1;
+    }
+    if (number === 2) {
+        return 1;
+    }
+    if (number > 1) {
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 };
 
 // Do not edit below this line
